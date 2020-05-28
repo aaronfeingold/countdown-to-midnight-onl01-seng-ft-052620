ef99b263ce862a2e5b67682fa372d3493ef303f3
Aaron Feingold <ajfeingold88@gmail.com>
2020-05-28 03:24:11 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
 #write your code here
 
-def countdown
+require 'pry'
+
+n = 10
+
+def countdown (n)
+   until n == 0   
+    puts "#{n} SECOND(S)!"
+    n -= 1
+   end
+   return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep (n)
+  until n == 0
+    sleep 5
+    n -= 1
+  end
+end
+
