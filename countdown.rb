3f974bb04bc878dec6dfc0af7aa1fe78b3539373
Aaron Feingold <ajfeingold88@gmail.com>
2020-05-28 03:14:36 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
 #write your code here
 
-def countdown
+require 'pry'
+
+n = 10
+
+def countdown (n)
+   until n == 0   
+    puts "#{n} SECOND(S)!"
+    n -= 1
+    puts "HAPPY NEW YEAR!"
+   end
 end
+
+
+
